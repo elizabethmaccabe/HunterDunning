f021fa1cfe964cf04493483f0a31e8cf58a26f36
Elizabeth MacCabe <2macce32@solent.ac.uk>
2018-02-26 16:17:28 +0000

    Installed Autoprefixer and added to Gulpfile.js
    Postprocessor for CSS to automatically add prefixes for browser support

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,9 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('styles', function(){
     gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
         .pipe(gulp.dest('./assets/css/'));
 });
 
