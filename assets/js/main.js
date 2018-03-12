2a6e941a4e5454f401e0010e959bc16cf2830bf8
Elizabeth MacCabe <2macce32@solent.ac.uk>
2018-03-12 13:08:30 +0000

    Home and Job Pages Completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,77 +1,35 @@
-//----------- Onclick form dropdown list function ----------//
-$(document).ready(function()
+// [START] Hamburger Menu Overlay Click Functions //
+function openNav()
 {
-    //----------- [start] job type dropdown function ----------//
-    $('.dl-parent-type').on("click",function()
+    document.getElementById("main-nav").style.height = "100%";
+}
+
+function closeNav()
+{
+    document.getElementById("main-nav").style.height = "0%";
+}
+// [END] //
+
+
+// [START] Homepage Nav Dropdown Functionality //
+$(function()
+{
+    $('li.dropdown > a').on('click',function(event)
     {
-        $(this).find(".dl-content-type").toggle();
-        $(this).siblings().find(".dl-content-type").hide();
+        event.preventDefault()
+        $(this).parent().find('ul').first().toggle(300);
+        $(this).parent().siblings().find('ul').hide(200);
 
-        if($(".dl-content-type:visible").length === 0 )
-        {
-            $("#dl-overlay-type").hide();
-        }
-        else
-        {
-            $("#dl-overlay-type").show();
-        }
+        //Hide menu when clicked outside
+        $(this).parent().find('ul').mouseleave(function()
+        {  
+            var thisUI = $(this);
+            $('html').click(function()
+            {
+                thisUI.hide();
+                $('html').unbind('click');
+            });
+        });
     });
-
-    $("#dl-overlay-type").on("click",function()
-    {
-        $(".dl-content-type").hide();
-        $(this).hide();
-    });
-    //----------- [end] job type dropdown function ----------//
-
-    
-    
-    //----------- [start] category dropdown function ----------//
-    $('.dl-parent-category').on("click",function()
-    {
-        $(this).find(".dl-content-category").toggle();
-        $(this).siblings().find(".dl-content-category").hide();
-
-        if($(".dl-content-category:visible").length === 0 )
-        {
-            $("#dl-overlay-category").hide();
-        }
-        else
-        {
-            $("#dl-overlay-category").show();
-        }
-    });
-    //----------- [end] category dropdown function ----------//
-
-    
-    
-    //----------- [start] location dropdown function ----------//
-    $("#dl-overlay-category").on("click",function()
-    {
-        $(".dl-content-category").hide();
-        $(this).hide();
-    });
-
-    $('.dl-parent-location').on("click",function()
-    {
-        $(this).find(".dl-content-location").toggle();
-        $(this).siblings().find(".dl-content-location").hide();
-
-        if($(".dl-content-location:visible").length === 0 )
-        {
-            $("#dl-overlay-location").hide();
-        }
-        else
-        {
-            $("#dl-overlay-location").show();
-        }
-    });
-
-    $("#dl-overlay-location").on("click",function()
-    {
-        $(".dl-content-location").hide();
-        $(this).hide();
-    });
-    //----------- [end] location dropdown function ----------//
 });
-//----------- [end] entire dropdown list function ----------//
+// [END] //
