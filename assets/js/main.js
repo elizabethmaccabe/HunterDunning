125dd0ef2143d4cfd36f61a760dcddecd869fc51
Elizabeth MacCabe <2macce32@solent.ac.uk>
2018-03-14 21:23:04 +0000

    Updated File Structure

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,27 +9,3 @@ function closeNav()
     document.getElementById("main-nav").style.height = "0%";
 }
 // [END] //
-
-
-// [START] Homepage Nav Dropdown Functionality //
-$(function()
-{
-    $('li.dropdown > a').on('click',function(event)
-    {
-        event.preventDefault()
-        $(this).parent().find('ul').first().toggle(300);
-        $(this).parent().siblings().find('ul').hide(200);
-
-        //Hide menu when clicked outside
-        $(this).parent().find('ul').mouseleave(function()
-        {  
-            var thisUI = $(this);
-            $('html').click(function()
-            {
-                thisUI.hide();
-                $('html').unbind('click');
-            });
-        });
-    });
-});
-// [END] //
